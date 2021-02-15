f2e1634958d51c091c3ba9f5d5bc9e58a50406df
takdongwan <dongwantak@gmail.com>
2021-02-15 17:44:53 +0900

    업로드 form , drop-zone 생성

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -21,7 +21,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/register" component={Auth(VideoUploadPage, false)} />
+          <Route exact path="/video/upload" component={Auth(VideoUploadPage, false)} />
         </Switch>
       </div>
       <Footer />
