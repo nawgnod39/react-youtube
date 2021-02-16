6e409425ce64533c8e8ea0fee35232adcae280b6
takdongwan <dongwantak@gmail.com>
2021-02-16 21:34:48 +0900

    ffmpeg setting

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
+
 const { Video } = require("../models/Video");
 const { auth } = require("../middleware/auth");
 
@@ -8,10 +10,10 @@ const { auth } = require("../middleware/auth");
 
 
 var storage = multer.diskStorage({
-    destination:  (req, file, cb) => {
+    destination: (req, file, cb) => {
         cb(null, 'uploads/')//저장하면 uploads 에  전부 저장이됨.
     },
-    filename:  (req, file, cb) => {
+    filename: (req, file, cb) => {
         cb(null, `${Date.now()}_${file.originalname}`)
     },
     fileFilter: (req, file, cb) => {
@@ -41,6 +43,40 @@ router.post("/uploadfiles", (req, res) => {
     })
 
 });
+
+router.post("/thumbnail", (req, res) => {
+
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+    })
+
+
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3,
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
+
+});
+
 
 
 
