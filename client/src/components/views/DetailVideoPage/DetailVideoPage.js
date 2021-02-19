4854b2f019fa3a22d80efe3c7580f0f1cade0015
takdongwan <dongwantak@gmail.com>
2021-02-19 16:32:36 +0900

    비디오 페이지에 side 비디오 생성

--- client/src/components/views/DetailVideoPage/DetailVideoPage.js
+++ client/src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { List, Avatar, Typography } from 'antd';
+import { List, Avatar, Row, Col } from 'antd';
 import axios from 'axios';
-
+import SideVideo from './Sections/SideVideo';
 function DetailVideoPage(props) {
 
 
@@ -24,24 +24,43 @@ function DetailVideoPage(props) {
             })
     }, [])
 
+    if(Video.writer) {
+        return (
+            <Row>
+                <Col lg={18} xs={24}>
+                    <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+                        <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
 
-    return (//없을
-        <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
-            <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
+                        <List.Item
+                            actions={[ ]}
+                        >
+                            <List.Item.Meta
+                                avatar={<Avatar src={Video.writer && Video.writer.image} />}
+                                title={<a href="https://ant.design">{Video.title}</a>}
+                                description={Video.description}
+                            />
+                            <div></div>
+                        </List.Item>
 
-            <List.Item
-                actions={[]}
-            >
-                <List.Item.Meta
-                    avatar={<Avatar src={Video.writer && Video.writer.image} />}//유저의 이미지 
-                    title={<a href="https://ant.design">{Video.title}</a>}
-                    description={Video.description}
-                />
-                <div></div>
-            </List.Item>
+                    </div>
+                </Col>
+                <Col lg={6} xs={24}>
 
-        </div>
-    )
+                    <SideVideo />
+
+                </Col>
+            </Row>
+        )
+
+    } else {
+        return(
+            <div>Loading...</div>
+        )
+    }
+
+
 }
 
 export default DetailVideoPage
+
+
