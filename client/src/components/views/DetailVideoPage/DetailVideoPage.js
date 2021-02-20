38e5d4423e9f159ccc6d257d451a9417772fa302
takdongwan <dongwantak@gmail.com>
2021-02-20 17:57:26 +0900

    Subscriber Feature

--- client/src/components/views/DetailVideoPage/DetailVideoPage.js
+++ client/src/components/views/DetailVideoPage/DetailVideoPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { List, Avatar, Row, Col } from 'antd';
 import axios from 'axios';
 import SideVideo from './Sections/SideVideo';
+import Subscriber from './Sections/Subscriber';
 function DetailVideoPage(props) {
 
 
@@ -32,7 +33,7 @@ function DetailVideoPage(props) {
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls></video>
 
                         <List.Item
-                            actions={[ ]}
+                            actions={[ <Subscriber userTo={Video.writer._id} userFrom={localStorage.getItem('userId')} /> ]}
                         >
                             <List.Item.Meta
                                 avatar={<Avatar src={Video.writer && Video.writer.image} />}
@@ -47,6 +48,7 @@ function DetailVideoPage(props) {
                 <Col lg={6} xs={24}>
 
                     <SideVideo />
+                    
 
                 </Col>
             </Row>
